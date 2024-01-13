fc663b51731561eef3fe3e6668eccd55c7a8f1cb
Tudor Anastasiu <tg.anastasiu@gmail.com>
2024-01-13 15:49:15 +0200

    chore: rename level 8 to level 7

--- test/7_Force.t.sol
+++ test/7_Force.t.sol
@@ -1,22 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "forge-std/Script.sol";
-import "../src/8_ForceSolution.sol";
+import "forge-std/Test.sol";
+import "../src/7_ForceSolution.sol";
 
 address constant INSTANCE = 0xB7690132c2236b969d19931f87e839870c72c7bB;
 
-contract ForcePOCScript is Script {
-    function run() external {
+contract ForcePOCTest is Test {
+    function test() external {
         vm.startBroadcast();
-
-        assert(address(INSTANCE).balance == 0);
+        assertEq(address(INSTANCE).balance, 0);
 
         ForceSolution solution = new ForceSolution();
         solution.solve{value: 1 wei}(0xB7690132c2236b969d19931f87e839870c72c7bB);
 
-        assert(address(INSTANCE).balance > 0);
-
+        assertNotEq(address(INSTANCE).balance, 0);
         vm.stopBroadcast();
     }
 }
