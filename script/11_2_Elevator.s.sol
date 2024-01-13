194166817ae96a4bdfdff95784e897c8655b0050
Tudor Anastasiu <tg.anastasiu@gmail.com>
2024-01-13 17:31:35 +0200

    feat: add level 12 - Elevator alternative solution

---
+++ script/11_2_Elevator.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "forge-std/Script.sol";
+import "../src/interfaces/11_IElevator.sol";
+import "../src/11_2_ElevatorSolution.sol";
+
+contract ElevatorPOCScript is Script {
+    IElevator instance = IElevator(INSTANCE);
+    
+    function run() external {
+        vm.startBroadcast();
+
+        assert(!instance.top());
+
+        ElevatorSolution solution = new ElevatorSolution();
+        solution.solve{gas: 100000}();
+
+        assert(instance.top());
+
+        vm.stopBroadcast();
+    }
+}
